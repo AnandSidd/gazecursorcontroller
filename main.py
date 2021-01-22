97216c6520cb3ccf48d5d16dc90dbcd8e1f1fe58
AnandSidd <siddharth.asrivastav.ece19@itbhu.ac.in>
2021-01-23 00:03:03 +0530

    facial landmark detection updated

--- main.py
+++ main.py
@@ -5,16 +5,25 @@ fd = MTCNN()
 
 
 def facedetection(frame):
-    frame = cv2.resize(frame, (600, 400))
     box = fd.detect_faces(frame)
+    print(box)
     head_image = []
     if box:
         b = box[0]['box']
         conf = box[0]['confidence']
         x, y, w, h = b[0], b[1], b[2], b[3]
-        head_image = frame[x:x+w, y:y+h]
+        l_e = box[0]['keypoints']['left_eye']
+        r_e = box[0]['keypoints']['right_eye']
+        nose = box[0]['keypoints']['nose']
         if conf > 0.5:
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 1)
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            head_image = frame[y:y+h,x:x+w]
+            cv2.circle(frame, nose, 2, (0, 255, 0), thickness=5, lineType=8, shift=0)
+
+            cv2.rectangle(frame, (int(r_e[0]) - 30, int(r_e[1]) - 30),
+                          (int(r_e[0]) + 30, int(r_e[1]) + 30), (255, 255, 255), 2)
+            cv2.rectangle(frame, (int(l_e[0]) - 30, int(l_e[1]) - 30),
+                          (int(l_e[0]) + 30, int(l_e[1]) + 30), (255, 255, 255), 2)
 
     return box, head_image, frame
 
@@ -22,8 +31,10 @@ def main():
     cap = cv2.VideoCapture(0)
     while (True):
         ret, frame = cap.read()
+        frame = cv2.resize(frame, (600, 400))
         box, head, detectframe = facedetection(frame)
-        cv2.imshow("Frame", detectframe)
+        if len(head)!=0:
+            cv2.imshow("Frame", detectframe)
         if cv2.waitKey(25) & 0xFF == ord('q'):
             break
 
